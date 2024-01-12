1e239fb0c9e023252e17fe79425ab339dd16ecd8
KÜBRA MOĞUL <kubramogul06@gmail.com>
2024-01-12 23:12:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SinavNotuHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbAd = new System.Windows.Forms.TextBox();
+            this.txbSoyad = new System.Windows.Forms.TextBox();
+            this.txbSinav2 = new System.Windows.Forms.TextBox();
+            this.txbSinav1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbProje = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(7, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad:";
+            // 
+            // txbAd
+            // 
+            this.txbAd.Location = new System.Drawing.Point(69, 5);
+            this.txbAd.Name = "txbAd";
+            this.txbAd.Size = new System.Drawing.Size(100, 20);
+            this.txbAd.TabIndex = 2;
+            // 
+            // txbSoyad
+            // 
+            this.txbSoyad.Location = new System.Drawing.Point(69, 45);
+            this.txbSoyad.Name = "txbSoyad";
+            this.txbSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txbSoyad.TabIndex = 3;
+            // 
+            // txbSinav2
+            // 
+            this.txbSinav2.Location = new System.Drawing.Point(216, 115);
+            this.txbSinav2.Name = "txbSinav2";
+            this.txbSinav2.Size = new System.Drawing.Size(48, 20);
+            this.txbSinav2.TabIndex = 7;
+            // 
+            // txbSinav1
+            // 
+            this.txbSinav1.Location = new System.Drawing.Point(75, 114);
+            this.txbSinav1.Name = "txbSinav1";
+            this.txbSinav1.Size = new System.Drawing.Size(44, 20);
+            this.txbSinav1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(148, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sınav 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(7, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Sınav 1:";
+            // 
+            // txbProje
+            // 
+            this.txbProje.Location = new System.Drawing.Point(359, 114);
+            this.txbProje.Name = "txbProje";
+            this.txbProje.Size = new System.Drawing.Size(42, 20);
+            this.txbProje.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(305, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Proje:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 232);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(405, 95);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 339);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txbProje);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbSinav2);
+            this.Controls.Add(this.txbSinav1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbSoyad);
+            this.Controls.Add(this.txbAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbAd;
+        private System.Windows.Forms.TextBox txbSoyad;
+        private System.Windows.Forms.TextBox txbSinav2;
+        private System.Windows.Forms.TextBox txbSinav1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbProje;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinavNotuHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = txbAd.Text;
+            string soyad = txbSoyad.Text;
+            double sinav1=Convert.ToDouble(txbSinav1.Text);
+            double sinav2 = Convert.ToDouble(txbSinav2.Text);
+            double proje = Convert.ToDouble(txbProje.Text);
+            double ort = (sinav1 + sinav2 + proje)/3;
+            listBox1.Items.Add(ad+" "+soyad+" "+"Ortalama= "+ort);
+        }
+    }
+}
